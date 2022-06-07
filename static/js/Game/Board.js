d343cefbf656b212a742d368c15c391668a75679
Mateusz-Obirek <95253738+Mateusz-Obirek@users.noreply.github.com>
2022-06-07 23:38:01 +0200

    Create Board.js

---
+++ static/js/Game/Board.js
@@ -0,0 +1,10 @@
+class Board{
+    constructor(){
+        this.items = []
+        for(let y=0; y<3; y++){
+            for(let x=0; x<3; x++){
+                this.items.push(new THREE)
+            }
+        }
+    }
+}
