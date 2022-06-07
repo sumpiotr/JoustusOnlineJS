c3b0d69444254957bc2f3bafdc075c3ecaa857ac
sumpiotr <sumpiotr@gmail.com>
2022-06-07 23:34:51 +0200

    Merge conflict fix

--- static/js/main.js
+++ static/js/main.js
@@ -5,22 +5,60 @@ const socket = io();
 
 //UI init
 uiManager.register("mainMenu");
+uiManager.register("waitingScreen");
+uiManager.register("privateGame");
 uiManager.display("mainMenu");
 
+const privateGameErrorInfo = document.getElementById("privateGameErrorInfo");
+
+//main menu
 uiManager.setOnSelectableClick("mainMenu", "tutorialButton", () => {
     window.open("https://www.yachtclubgames.com/blog/joustus-instruction-manual", "_blank");
 });
 
-//do usunięcia, tymczasowe
-uiManager.setOnSelectableClick('mainMenu', 'publicGameButton', ()=>{
-    uiManager.hide('mainMenu')
-})
+uiManager.setOnSelectableClick("mainMenu", "publicGameButton", () => {
+    socket.emit("createRoom", true, "publicGame", "");
+});
+
+uiManager.setOnSelectableClick("mainMenu", "privateGameButton", () => {
+    uiManager.display("privateGame");
+});
+
+//private Game
+uiManager.setOnSelectableClick("privateGame", "createGame", () => {
+    let name = uiManager.getSelectableValue("privateGame", "roomName");
+    let password = uiManager.getSelectableValue("privateGame", "roomPassword");
+
+    socket.emit("createRoom", false, name, password);
+});
+
+uiManager.setOnSelectableClick("privateGame", "joinGame", () => {
+    let name = uiManager.getSelectableValue("privateGame", "roomName");
+    let password = uiManager.getSelectableValue("privateGame", "roomPassword");
+
+    socket.emit("joinRoom", name, password);
+});
+
+//sockets
+socket.on("createRoom", (err) => {
+    if (err != "") {
+        privateGameErrorInfo.textContent = err;
+        return;
+    } else {
+        uiManager.display("waitingScreen");
+    }
+});
+
+socket.on("startGame", () => {
+    //start game
+    uiManager.hideAll();
+});
 
 //Game init
-gameManager.initCamera('mainCamera', 75, new THREE.Vector3(100, 100, 100))
-gameManager.setCamera('mainCamera')
+gameManager.initCamera("mainCamera", 75, new THREE.Vector3(100, 100, 100));
+gameManager.setCamera("mainCamera");
 
-gameManager.initScene('myScene1')
-gameManager.setFocus('myScene1')
+gameManager.initScene("myScene1");
+gameManager.setFocus("myScene1");
 
-gameManager.startRenderer()
+gameManager.startRenderer();
